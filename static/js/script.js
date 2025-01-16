76400bf95ced714085ce90e44584db8891767b28
Lucas Henrique de Barros <128721960+lucashebarros@users.noreply.github.com>
2025-01-16 00:10:22 -0300

    Create script.js

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('/api/data')
+    .then(response => response.json())
+    .then(data => {
+        const map = L.map('map').setView([-23.55052, -46.633308], 4);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        data.forEach(record => {
+            const marker = L.marker([record.point_of_sale.lat, record.point_of_sale.lon]).addTo(map);
+            marker.bindPopup(`
+                <b>Vehicle:</b> ${record.vehicle}<br>
+                <b>Time:</b> ${record.transaction_date}<br>
+                <b>Location:</b> ${record.point_of_sale.name}<br>
+                <b>City:</b> ${record.point_of_sale.city}, ${record.point_of_sale.state}<br>
+                <b>Product:</b> ${record.product}<br>
+                <b>Quantity:</b> ${record.quantity.toFixed(2)} L<br>
+                <b>Unit Price:</b> R$${record.unit_price.toFixed(2)}<br>
+                <b>Total Price:</b> R$${record.total_price.toFixed(2)}
+            `);
+        });
+    });
+});
